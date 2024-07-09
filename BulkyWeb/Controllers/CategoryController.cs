4c25eeefe128d225d2e63b2cffade7ff21d23310
Thành Đạt Lưu <luuthanhdat19022021@gmail.com>
2024-07-09 09:48:29 +0700

    first commit

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly DataContext _context;
+
+        public CategoryController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var categoryList = _context.Categories.ToList();
+
+            return View(categoryList);
+        }
+
+		public IActionResult Create() => View();
+
+		[HttpPost]
+		public IActionResult Create(Category category)
+		{
+            if(category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Display Order cannot match Name");
+            }
+
+            if (!ModelState.IsValid) return View();
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
+}
